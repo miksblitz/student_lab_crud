e0174d2fd51f809e8d5e3e637e70f41c129efc49
miksblitz <mikelaboyme@gmail.com>
2026-02-04 04:01:59 +0800

    updated

--- db_connect.php
+++ db_connect.php
@@ -2,11 +2,18 @@
 $host = "localhost";
 $user = "root";  // default in XAMPP/WAMP
 $pass = "";
-$db   = "student_db";
+$db   = "crudlab";
 
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+$conn->query("CREATE TABLE IF NOT EXISTS students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    course VARCHAR(100) NOT NULL
+)");
 ?>
